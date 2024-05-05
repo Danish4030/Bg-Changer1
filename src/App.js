60b9107783ccb26677ccc3c9c211f139c318419f
Danish4030 <160005996+Danish4030@users.noreply.github.com>
2024-05-05 17:48:53 +0500

    bg- canger

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("white");
+
+  const handleColorClick = (newColor) => {
+    document.body.style.backgroundColor = newColor;
+    setColor(newColor);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="w-full h-screen duration-200 flex flex-col items-center justify-center">
+      <h1 className="text-center text-3xl text-white font-semibold mb-8">
+        Click on a color
+      </h1>
+      <div className="flex gap-4">
+        <button
+          onClick={() => handleColorClick("red")}
+          className="color-button"
+          style={{ backgroundColor: "red" }}
         >
-          Learn React
-        </a>
-      </header>
+          Red
+        </button>
+        <button
+          onClick={() => handleColorClick("green")}
+          className="color-button"
+          style={{ backgroundColor: "green" }}
+        >
+          Green
+        </button>
+        <button
+          onClick={() => handleColorClick("blue")}
+          className="color-button"
+          style={{ backgroundColor: "blue" }}
+        >
+          Blue
+        </button>
+        <button
+          onClick={() => handleColorClick("purple")}
+          className="color-button"
+          style={{ backgroundColor: "purple" }}
+        >
+          Purple
+        </button>
+        <button
+          onClick={() => handleColorClick("yellow")}
+          className="color-button"
+          style={{ backgroundColor: "yellow" }}
+        >
+          Yellow
+        </button>
+        <button
+          onClick={() => handleColorClick("pink")}
+          className="color-button"
+          style={{ backgroundColor: "pink" }}
+        >
+          Pink
+        </button>
+        <button
+          onClick={() => handleColorClick("olive")}
+          className="color-button"
+          style={{ backgroundColor: "olive" }}
+        >
+          Olive
+        </button>
+      </div>
     </div>
   );
 }
